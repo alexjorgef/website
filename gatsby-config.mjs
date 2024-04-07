2c78e3129eeb53883ca772716e18be41cf17dd3a
Alexandre Ferreira <alexjorgef@protonmail.com>
2024-04-07 13:54:21 +0100

    e2e tests and linting
    Signed-off-by: Alexandre Ferreira <alexjorgef@protonmail.com>

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -233,18 +233,9 @@ const gatsbyConfig = {
     {
       resolve: `@chakra-ui/gatsby-plugin`,
       options: {
-        /**
-         * @property {boolean} [resetCSS=true]
-         * if `false`, this plugin will not use `<CSSReset />
-         */
         resetCSS: true,
-        /**
-        * @property {number} [portalZIndex=40]
-        * The z-index to apply to all portal nodes. This is useful
-        * if your app uses a lot z-index to position elements.
-        */
-        portalZIndex: 40
-      }
+        portalZIndex: 40,
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
